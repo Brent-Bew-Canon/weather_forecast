f8b93a3127e76d5bb77b6cd01509e0c1d96584e9
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-03-27 14:02:02 -0700

    JS fetch for openweather api and geo api

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 
-weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=33.4484&lon=112.0740&appid=760dc2617eb1a39f1c3c50666743a368"
+let weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=33.4484&lon=-112.0740&appid=760dc2617eb1a39f1c3c50666743a368"
+
+let goeApi = "http://api.openweathermap.org/geo/1.0/direct?q=Phoenix&limit=1&appid=760dc2617eb1a39f1c3c50666743a368"
 
 fetch(weatherURL)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+
+fetch(goeApi)
     .then(function (response) {
         return response.json()
     })
