163ae7f0d42cb347b1383dafd9b326a18df3e14d
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-03-27 14:05:26 -0700

    JS parse lat and lon from city name

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 
 let weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=33.4484&lon=-112.0740&appid=760dc2617eb1a39f1c3c50666743a368"
 
-let goeApi = "http://api.openweathermap.org/geo/1.0/direct?q=Phoenix&limit=1&appid=760dc2617eb1a39f1c3c50666743a368"
+let geoApi = "http://api.openweathermap.org/geo/1.0/direct?q=Phoenix&limit=1&appid=760dc2617eb1a39f1c3c50666743a368"
 
-fetch(weatherURL)
+
+
+fetch(geoApi)
     .then(function (response) {
         return response.json()
     })
     .then(function (data) {
-        console.log(data);
+        console.log("lattitude: " + data[0].lat);
+        console.log("longitude: " + data[0].lon);
     })
 
-fetch(goeApi)
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (data) {
-        console.log(data);
-    })
+    // fetch(weatherURL)
+    // .then(function (response) {
+    //     return response.json()
+    // })
+    // .then(function (data) {
+    //     console.log(data);
+    // })
