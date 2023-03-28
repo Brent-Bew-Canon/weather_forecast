b31ce96a7b52c633bf48a37962130e74fdc3063e
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-03-27 18:00:58 -0700

    inlcuded dayjs and cleaned up excess js code

--- script.js
+++ script.js
@@ -1,62 +1,37 @@
 
 // "https://api.openweathermap.org/data/2.5/forecast?lat=33.4484&lon=-112.0740&appid=760dc2617eb1a39f1c3c50666743a368"
 let weatherApi = "https://api.openweathermap.org/data/2.5/forecast?"
-let geoApi = "http://api.openweathermap.org/geo/1.0/direct?"
-let geoUrl = ""
 let weatherUrl = ""
 let params = {
     q: "",
     limit: "",
-    lat: 0,
-    lon: 0,
     apiKey: "760dc2617eb1a39f1c3c50666743a368"
 }
 let city = document.querySelector("#city")
 
 // event listener for search button
 document.querySelector("#search").addEventListener("click", function () {
+
+    // conditional to stop process if cityname is blank
     if (city.value === "") {
         return
     } else {
-        // get the city name from the text input and build out the geoapi url with correct params
-        geoParams.q = city.value
-        geoUrl = geoApi + "q=" + params.q + "&limit=" + params.limit + "&appid=" + params.apiKey
-        console.log(geoUrl)
 
-        // Perform fetch through the geoapi with the city name to get long. and lat.
-        fetch(geoUrl)
-            .then(function (response) {
-                return response.json()
-            })
-            .then(function (data) {
+        // get the city name from the text input and build out the api url with correct params
+        params.q = city.value
+        weatherUrl = weatherApi + "q=" + params.q + "&appid=" + params.apiKey
+        console.log(weatherUrl)
 
-                // update the longitude and latitude parameters for the weather api url
-                weatherParams.lat = data[0].lat
-                weatherParams.lon = data[0].lon
-
-                //build the weather api url
-                weatherUrl = weatherApi + "lat=" + params.lat + "&lon=" + params.lon + "&appid=" + params.apiKey
-                console.log(weatherUrl)
-            })
+        // Perform fetch through the weather api
         fetch(weatherUrl)
             .then(function (response) {
-                console.log("response below")
-                console.log(response)
                 return response.json()
             })
             .then(function (data) {
-                console.log(data)
+                console.log(data.city.sunset)
             })
 
         document.querySelector("h2").textContent = city.value.toUpperCase()
     }
-
 })
 
-    // fetch(weatherURL)
-    // .then(function (response) {
-    //     return response.json()
-    // })
-    // .then(function (data) {
-    //     console.log(data);
-    // })
