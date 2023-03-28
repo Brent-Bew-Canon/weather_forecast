a5c95e5c11da8bf034818a9a997039a341621ba5
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-03-28 10:46:56 -0700

    captured correct days from api data

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let params = {
     limit: "",
     apiKey: "760dc2617eb1a39f1c3c50666743a368"
 }
+let forecast = [...Array(5)]
 let city = document.querySelector("#city")
 
 // event listener for search button
@@ -28,7 +29,26 @@ document.querySelector("#search").addEventListener("click", function () {
                 return response.json()
             })
             .then(function (data) {
-                console.log(data.city.sunset)
+
+                //sets time of day for the five day forecast based on time of query
+                let dayOne = dayjs.unix(data.list[0].dt)
+                dayOneHour = dayjs(dayOne).format("h a")
+                forecast[0] = dayjs(dayOne).format("M/DD/YYYY")
+                let count = 0
+
+                //loop through all the data to find the correct time each day
+                for (let i = 0; i < data.list.length; i++) {
+                    let time = dayjs.unix(data.list[i].dt)
+                    let parsed = dayjs(time).format("h a")
+                    console.log(parsed)
+
+                    //compare the time to the standard set above and add the data to the forecast array if it matches up
+                    if (parsed == dayOneHour) {
+                        forecast[count] = dayjs(time).format("M/DD/YYYY")
+                        count++
+                    }
+                }
+                console.log(forecast)
             })
 
         document.querySelector("h2").textContent = city.value.toUpperCase()
