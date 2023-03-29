e3f9aacd26bdc66aa4eba9d5e54add44246d8df9
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-03-29 11:30:47 -0700

    fixed search button css and added search history

--- script.js
+++ script.js
@@ -13,12 +13,14 @@ let temps = []
 let windList = []
 let humidity = []
 icons = []
+let cityname = ""
 
 let city = document.querySelector("#city")
 let currentTemp = document.querySelector("#current-temp")
 let currentWind = document.querySelector("#current-wind")
 let currentHumidity = document.querySelector("#current-humidity")
 let currentIcon = document.querySelector("#current-icon")
+let hr = document.querySelector("hr")
 
 
 // event listener for search button
@@ -41,14 +43,15 @@ document.querySelector("#search").addEventListener("click", function () {
             .then(function (data) {
                 console.log(data)
 
+                //save correct name of city to variable
+                cityname = data.city.name
+                console.log(cityname)
+
                 // this loop compares each data point against the first data point based on the day to get a list of all the 5 days beyond today
                 let relDates = []
                 for (let i = 0; i < data.list.length; i++) {
                     if (dayjs.unix(data.list[i].dt).format("M/DD") > dayjs.unix(data.list[0].dt).format("M/DD")) {
-                        console.log("greater day" + dayjs.unix(data.list[i].dt).format("M/DD"))
                         relDates.push(data.list[i])
-                    } else {
-                        console.log("not greater" + dayjs.unix(data.list[i].dt).format("M/DD"))
                     }
                     allDates[i] = data.list[i]
                 }
@@ -57,8 +60,6 @@ document.querySelector("#search").addEventListener("click", function () {
                 let dayOne = dayjs.unix(relDates[0].dt)
                 dayOneHour = dayjs(dayOne).format("h a")
                 forecast[0] = dayjs(dayOne).format("M/DD/YYYY")
-                console.log(forecast[0])
-                console.log(relDates)
 
                 //this loop goes through each of the 5 day forecast and formats it by the hour to determine which data points to grab so that they are all consistent by hour
                 let count = 0
@@ -68,21 +69,16 @@ document.querySelector("#search").addEventListener("click", function () {
 
                     //compares the hour against the first day/hour combo set above as dayOneHour and adds the data to the arrays if it matches up
                     if (parsed == dayOneHour) {
-                        console.log("This is a good day" + dayjs(time).format("M/DD/YYYY"))
                         temps.push(relDates[i].main.temp)
                         windList.push(relDates[i].wind.speed)
-                        console.log(windList[count])
                         humidity.push(relDates[i].main.humidity)
                         icons.push(relDates[i].weather[0].icon)
                         forecast[count] = dayjs.unix(relDates[i].dt).format("M/DD/YYYY")
                         count++
                     }
                 }
-                console.log(relDates)
-                console.log(forecast)
 
-
-                //loop to create elements and fill them with weather data
+                //loop to create card elements and fill them with weather data
                 for (let i = 0; i < 5; i++) {
 
                     //create elements
@@ -119,25 +115,37 @@ document.querySelector("#search").addEventListener("click", function () {
                     wind.textContent = "Wind: " + windList[i] + " MPH"
                     humid.textContent = "Humidity: " + humidity[i] + "%"
                     let iconId = icons[i]
-                    console.log(iconId);
                     iconImg.setAttribute("src", "https://openweathermap.org/img/wn/" + iconId + "@2x.png")
 
-
-                    currentTemp.textContent = "Temp: " + allDates[0].main.temp + "°"
-                    currentWind.textContent = "Wind: " + allDates[0].wind.speed + " MPH"
-                    currentHumidity.textContent = "Humidity: " + allDates[0].main.humidity + "%"
-                    currentIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + allDates[0].weather[0].icon + "@2x.png")
-
-                    // TODO: input the current icon into the img tag
-
-                    // TODO: Save previous search history to screen and local storage
-
-                    //TODO: Add functionality if city is not found
-
                 }
+                //fill in the weather elements with values for today
+                document.querySelector("h2").textContent = data.city.name + "(" + dayjs.unix(allDates[0].dt).format("M/DD/YYYY") + ")"
+                currentTemp.textContent = "Temp: " + allDates[0].main.temp + "°"
+                currentWind.textContent = "Wind: " + allDates[0].wind.speed + " MPH"
+                currentHumidity.textContent = "Humidity: " + allDates[0].main.humidity + "%"
+                currentIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + allDates[0].weather[0].icon + "@2x.png")
+
+                // TODO: Save previous search history to screen
+
+                //create and append history button and add class attributes
+                searchHistory = document.createElement("button")
+                hr.appendChild(searchHistory)
+                searchHistory.textContent = cityname
+                searchHistory.setAttribute("class", "history-button btn btn-secondary col-12 mt-4")
+
+                // Add event Listener that 
+
+                //TODO: Click on history runs the new search
+
+                //TODO: Add functionality if city is not found
+
+
+                //TODO: Delete cards when button is clicked
+
+
             })
 
-        document.querySelector("h2").textContent = city.value.toUpperCase()
+
     }
 })
 
